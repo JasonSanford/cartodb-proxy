772a2288eeaef0a52e5221f767c5a8bc9910f2c1
Jason Sanford <jasonsanford@gmail.com>
2015-08-20 22:10:08 -0400

    Fork tileify-ags-proxy. Seems to work.

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-var express = require('express'),
-    request = require('request'),
-    TileifyAGS = require('tileify-ags').TileifyAGS;
+var express = require('express');
+var request = require('request');
 
 var app = express();
 
@@ -11,30 +10,91 @@ app.get('/', function(req, resp) {
   resp.sendfile('public/index.html');
 });
 
-app.get('/tiles/:z/:x/:y', function(req, resp) {
-  var z = parseInt(req.params.z, 10);
-  var x = parseInt(req.params.x, 10);
-  var y = parseInt(req.params.y, 10);
+var cartodbConfigCache = {};
 
-  //pull out proxy query params
-  var ags_server_url = req.query.url;
+function sendBadRequestResponse (resp) {
+  resp.status(400);
+  resp.send('A "cartodb_json" url parameter must be passed and be valid JSON.')
+}
+
+function getLayerConfig (cartodbOrg, cartodbConfig, callback) {
+  var cacheKey = JSON.stringify(cartodbConfig);
+
+  if (cacheKey in cartodbConfigCache) {
+    if (cartodbConfigCache[cacheKey] === 'fetching') {
+      console.log('cache miss during initial fetch');
+      setTimeout(function () {
+        getLayerConfig(cartodbOrg, cartodbConfig, callback);
+      }, 200);
+    } else {
+      console.log('cache hit');
+      callback(null, cartodbConfigCache[cacheKey]);
+    }
+  } else {
+    console.log('cache miss');
+    cartodbConfigCache[cacheKey] = 'fetching';
+    request({
+        url: 'https://' + cartodbOrg + '.cartodb.com/api/v1/map',
+        method: 'post',
+        json: true,
+        body: cartodbConfig
+      }, function (error, response, layerConfig) {
+        if (error) {
+          callback(error);
+        } else {
+          cartodbConfigCache[cacheKey] = layerConfig;
+          callback(null, layerConfig);
+        }
+      });
+  }
+}
+
+app.get('/tiles/:z/:x/:y', function(req, resp) {
+  var z                  = parseInt(req.params.z, 10);
+  var x                  = parseInt(req.params.x, 10);
+  var y                  = parseInt(req.params.y, 10);
+  var cartodbOrg         = req.query.cartodb_org;
+  var encodedCartodbJson = req.query.cartodb_json;
+
+  if (!encodedCartodbJson) {
+    sendBadRequestResponse(resp);
+    return;
+  }
+
+  try {
+    var cartodbJson = decodeURIComponent(encodedCartodbJson);
+    var cartodbConfig = JSON.parse(cartodbJson);
+  } catch (error) {
+    sendBadRequestResponse(resp);
+    return;
+  }
+
   var redirect = req.query.redirect != null && req.query.redirect.toLowerCase() !== "false" ? true : false;
 
-  //grab all other query params intended for the ags server
-  var url_param_config = req.query;
-  delete url_param_config.url;
-  delete url_param_config.redirect;
+  getLayerConfig(cartodbOrg, cartodbConfig, function (error, layerConfig) {
+    var url;
 
-  //create the tile url
-  var tiler = new TileifyAGS(url_param_config);
-  var url = tiler.getTileUrl(ags_server_url, x, y, z);
+    if (error) {
+      console.log('Error: ', error);
+      url = 'http://i.imgur.com/w9RAwBL.gif';
+    } else {
+      if (layerConfig.layergroupid && layerConfig.cdn_url) {
+        var layerGroupId  = layerConfig.layergroupid;
+        var cartodbServer = layerConfig.cdn_url.https;
 
-  //serve it up!
-  if (redirect) {
-    resp.redirect(url);
-  } else {
-    req.pipe(request(url)).pipe(resp);
-  }
+        url = 'https://' + cartodbServer + '/' + cartodbOrg + '/api/v1/map/' + layerGroupId +  '/' + z +  '/' + x + '/' + y + '.png';
+      } else {
+        console.log('Bad layerConfig: ', layerConfig);
+        url = 'http://i.imgur.com/w9RAwBL.gif';
+      }
+    }
+
+    if (redirect) {
+      resp.redirect(url);
+    } else {
+      req.pipe(request(url)).pipe(resp);
+    }
+  });
 });
 
 app.listen(app.get('port'), function() {
--- public/app.js
+++ public/app.js
@@ -1,40 +1,37 @@
 (function(){
   var base_layer = new L.TileLayer('https://{s}.tiles.mapbox.com/v4/base.mapbox-streets+bg-e8e8e8_scale-1_water-0.13x0.13;0.00x0.00;0.81x0.81;0.00x1.00_streets-0.08x0.08;0.00x0.00;0.11x1.00;0.00x1.00_landuse-0.10x0.10;0.00x0.00;0.76x0.98;0.00x1.00_buildings-0.08x0.08;0.00x0.00;0.11x1.00;0.00x1.00/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6IlhHVkZmaW8ifQ.hAMX5hSW-QnTeRCMAy9A8Q', {subdomains: 'abcd'});
   var map_options = {
-    center: [35.0556188, -80.664826],
-    zoom: 17,
+    center: [27.7720769, -82.6420752],
+    zoom: 16,
     layers: [base_layer],
     maxZoom: 21
   };
   var map = new L.Map($('.map')[0], map_options);
 
-  var ags_layer;
+  var cartoDBLayer;
 
   function updateLayer(event) {
     if (event) {
       event.preventDefault();
     }
+
     var url = window.location.origin + '/tiles/{z}/{x}/{y}';
+
     var params = (function() {
-      var encoded_ags_url = window.encodeURIComponent($('#ags_url').val());
-      var key_vals = ['url=' + encoded_ags_url];
-      $('.params').each(function (index, object) {
-        var $param = $(object);
-        var key = $param.find('.param-key').val();
-        var value = $param.find('.param-value').val();
-        if (key.length && value.length) {
-          key_vals.push(key + '=' + window.encodeURIComponent(value));
-        }
-      });
+      var encoded_cartodb_json = window.encodeURIComponent($('#cartodb_json').val());
+      var key_vals = ['cartodb_json=' + encoded_cartodb_json];
+      key_vals.push('cartodb_org=' + $('#cartodb_org').val())
       return key_vals;
     }());
+
     var url_template = url + '?' + params.join('&');
     $('#proxy_url_template').val(url_template);
-    if (ags_layer) {
-      ags_layer.setUrl(url_template);
+
+    if (cartoDBLayer) {
+      cartoDBLayer.setUrl(url_template);
     } else {
-      ags_layer = new L.TileLayer(url_template, {maxZoom: 21});
-      map.addLayer(ags_layer);
+      cartoDBLayer = new L.TileLayer(url_template, {maxZoom: 21});
+      map.addLayer(cartoDBLayer);
     }
   }
 
